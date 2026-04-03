d15f8c1dfe1f804733bfad5a34e03e4e361182a8
Печенин Вадим Андреевич <pechenin.va@ssau.ru>
2026-04-03 13:55:28 +0400

    Add backend docker (db external docker)

--- FAserver/app/db/config/config.py
+++ FAserver/app/db/config/config.py
@@ -1,5 +1,6 @@
-DATABASE_URI = 'postgresql+psycopg2://postgres:mapr@localhost:5432/lfc'
-
+#DATABASE_URI = 'postgresql+psycopg2://postgres:mapr@localhost:5432/lfc'
+#Для Docker
+DATABASE_URI = 'postgresql+psycopg2://postgres:mapr@host.docker.internal:5432/lfc'
 nameOfDataBase = "lfc"
 
 SQLDataBaseObj = None
--- FAserver/deploy/sentence_transformers_cache_filling.py
+++ FAserver/deploy/sentence_transformers_cache_filling.py
@@ -5,7 +5,7 @@ from pathlib import Path
 def main():
     model_name = "paraphrase-multilingual-MiniLM-L12-v2"
     cache_folder = str(Path(__file__).resolve().parent.parent
-                       .joinpath("test").joinpath("resources").joinpath("sentence_transformers_cache").resolve())
+                       .joinpath("tests").joinpath("resources").joinpath("sentence_transformers_cache").resolve())
 
     model = SentenceTransformer(model_name)
     model.save(cache_folder)
